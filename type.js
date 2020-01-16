cb30a28e70efe965eaa0c24cc30bcf69995da674
Steffan Jones <SteffanDrosinos@outlook.com>
2020-01-16 22:53:55 +0000

    styling changes

--- type.js
+++ type.js
@@ -6,16 +6,21 @@ var correct = 0;
 var incorrect = 0;
 var text;
 var best_score = -1;
+var last_index = -1;
+var char_count = -1;
 
 $(function () {
     getText();
     updateBestScore();
+    char_count = getCharecterCount();
+    console.log("char count: " + char_count);
 });
 
 function getText() {
-    var rand = Math.floor((Math.random() * 61));
-    text = texts[rand];
+    var text_id = Math.floor((Math.random() * 62));
+    text = texts[text_id];
     $("#text").html(getTextDiv());
+    $("#text_id").html("<gr>id: <super>" + text_id + "</super></gr>");
 }
 
 function getTextDiv() {
@@ -32,6 +37,14 @@ function checkSpelling(index, input) {
     } else return false;
 }
 
+function getCharecterCount() {
+    var count = 0;
+    for (var i=0; i<text.length; i++) {
+        count += text[i].length;
+    }
+    return count
+}
+
 function updateSentence(correct, index) {
     if (correct) {
         var id = "#text_" + index;
@@ -39,6 +52,18 @@ function updateSentence(correct, index) {
     } else {
         var id = "#text_" + index;
         $(id).css("color", "#FF2222");
+    }
+}
+
+function updateProgress(word_index) {
+    if (word_index != 0) {
+        var percent_done = (word_index / text.length) * 100;
+        var str = "" + percent_done.toFixed(0) + "%";
+        $("#prog_val").css("width", str);
+        $("#prog_show").css("left", str);
+    } else {
+        $("#prog_val").css("width", "0%");
+        $("#prog_show").css("left", "0%");
     }
 }
 
@@ -78,7 +103,8 @@ function reset() {
 }
 
 function resetProgress() {
-
+    $("#prog_val").css("width", "0%");
+    $("#prog_show").css("left", "0%");
 }
 
 $(document).keydown(function (e) {
@@ -95,6 +121,7 @@ $(document).keydown(function (e) {
         timer_start = false;
         resetProgress();
     }
+
     switch (e.keyCode) {
         case 32: //space key
             e.preventDefault();
@@ -108,6 +135,8 @@ $(document).keydown(function (e) {
                 updateSentence(spelling, word_index);
             }
 
+            updateProgress(word_index);
+
             if (word_index == text.length) {
                 var wpms = text.length / mseconds;
                 var wpm = wpms * 60000;
@@ -116,14 +145,13 @@ $(document).keydown(function (e) {
                     localStorage['best_score'] = wpm.toFixed(2);
                 }
                 printResults(wpm);
-
                 reset();
             }
             break;
 
         case 13:
             reset();
-
+            resetProgress()
         default:
             //do nothing
             break;
